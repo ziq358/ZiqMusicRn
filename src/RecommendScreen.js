501c977f145d9b5bfd93ac1c61461a1fc48377b6
jwu <jwu@xogrp.com>
2018-01-25 00:19:30 +0800

    navigate to next page

--- src/RecommendScreen.js
+++ src/RecommendScreen.js
@@ -12,9 +12,12 @@ export default class RecommendScreen extends Component<{}> {
     return (
       <View style={{ flex: 1, backgroundColor: '#1F1F1F'}}>
         <View style={{ flex: 1, maxHeight: 50, /*flex 会影响 宽高的设置， 用maxHeight 限制*/
-                        backgroundColor: '#eb776a' }}//背景 
+                        backgroundColor: '#eb776a' }}//背景
                         />{/* 一般注释, 用 {} 包围 */}
-                        <Text style={{ color: '#ffffff'}}>RecommendScreen</Text>
+            <Text style={{ color: '#ffffff'}} onPress={()=>{
+              const {navigate} = this.props.navigation;/*需要以这种方式来调方法，需要大括号*/
+              navigate('SongListScreen');
+            }}>RecommendScreen</Text>
         <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />{/* 一般注释, 用 {} 包围 */}
       </View>
     );
